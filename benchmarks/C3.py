1e94efe5e7ba29b6bcd1c9e0e2fcff768bcbd938
blliu6 <lmd20010321@gmail.com>
2025-01-19 17:07:18 +0800

    update C3

--- benchmarks/C3.py
+++ benchmarks/C3.py
@@ -27,15 +27,15 @@ def main():
         'b1_hidden': b1_hidden_neurons,
         "example": example,
         "batch_size": 1000,
-        'lr': 0.5,
+        'lr': 0.4,
         'loss_weight_continuous': (1, 1, 1),
         'R_b': 0.1,
         'margin': 2,
         "DEG_continuous": [2, 2, 1, 2],
         "learning_loops": 100,
         'max_iter': 10,
-        'counterexamples_ellipsoid': True,
-        'path':path
+        # 'counterexamples_ellipsoid': True,
+        'path': path
     }
     Config = CegisConfig(**opts)
     cegis = Cegis(Config)
